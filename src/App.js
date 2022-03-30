cf0658d0787814a2b6c5a8656506512c4ed245ea
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-29 22:59:58 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import zaddy from "./zaddy.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={zaddy} className="App-logo img-fluid" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
