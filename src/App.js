b59efab94045f2bbad294bc5aa7133d3fbcdc61c
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-30 22:26:42 -0700

    Added Search Engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import zaddy from "./zaddy.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={zaddy} className="App-logo img-fluid" alt="logo" />
+      <div className="container">
+        <header className="App-header">
+          <img src={zaddy} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer text-center">
+          <small>Coded by Claire Fleischer</small>
+        </footer>
+      </div>
     </div>
   );
 }
