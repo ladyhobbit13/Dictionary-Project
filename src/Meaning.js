74872dc0caac6a8292462cdbec513e19d1918724
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-04-02 20:46:34 -0700

    Expanded Meaning results

---
+++ src/Meaning.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h5>{props.meaning.partOfSpeech}</h5>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>{definition.definition}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
