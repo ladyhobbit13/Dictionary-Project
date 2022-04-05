8145cf772bcfbba58fbdc005457174e7e71e5481
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-04-04 20:01:25 -0700

    Added Synonym component and added to Meaning

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,7 +8,11 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+              <Synonyms synonyms={definition.synonyms} />
+            </p>
           </div>
         );
       })}
