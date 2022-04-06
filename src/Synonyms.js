8c98311e466f2ca5a96b246af1b61c000a978948
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-04-05 23:01:20 -0700

    Added Phonetics

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -5,7 +5,12 @@ export default function Synonyms(props) {
     return (
       <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          return (
+            <span className="Synonym">
+              <strong>Synonyms</strong>
+              <li key={index}>{synonym}</li>
+            </span>
+          );
         })}
       </ul>
     );
